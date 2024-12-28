b79cbc69a152d00509e28f79bd6345dbd79e2ff8
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-12-28 19:43:33 +0530

    Designed our designers section

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,1 +1,10 @@
-export {};
+import { Designer } from "../typescript/interfaces/interface.types";
+import designer1 from '../assets/images/designer-1.png';
+import designer2 from '../assets/images/designer-2.png';
+import designer3 from '../assets/images/designer-3.png';
+
+export const designers: Designer[] = [
+    { id: 1, image: designer1, name: 'One', role: 'Barber' },
+    { id: 2, image: designer2, name: 'Two', role: 'Lead Designer & Barber' },
+    { id: 3, image: designer3, name: 'Three', role: 'Designer' },
+];
--- src/typescript/interfaces/interface.types.ts
+++ src/typescript/interfaces/interface.types.ts
@@ -1,1 +1,6 @@
-export {};
+export interface Designer {
+    id: number,
+    image: string,
+    role: string,
+    name: string,
+}
